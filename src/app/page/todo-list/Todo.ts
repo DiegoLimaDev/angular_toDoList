9d641da06b6b92c09101024bf333df0b9458269f
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-30 13:04:36 -0300

    [feat]todo List component

---
+++ src/app/page/todo-list/Todo.ts
@@ -0,0 +1,9 @@
+export class Todo {
+  name: string;
+  isCompleted: boolean;
+
+  constructor(name: string, isCompleted: boolean) {
+    this.name = name;
+    this.isCompleted = isCompleted;
+  }
+}
---
+++ src/app/page/todo-list/todo-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Todo } from './Todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+})
+export class TodoListComponent {
+  todos: Todo[] = [];
+  newTodo = '';
+
+  saveTodo() {
+    if (this.newTodo) {
+      const todo = new Todo(this.newTodo, true);
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Please enter a todo task');
+    }
+  }
+
+  doneTodo(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  removeTodo(id: number) {
+    this.todos = this.todos.filter((e, i) => i !== id);
+  }
+}
